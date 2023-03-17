c45485b6eedde15e7e417defb99beb9cda43fb6b
gdsafgd <102455578+gdsafgd@users.noreply.github.com>
2023-03-17 18:29:32 +0900

    메인화면

--- 남우혁/js/index.js
+++ 남우혁/js/index.js
@@ -1,60 +1,132 @@
-var imgeIndex = 1;
-function changePotalImg() {
-  $('#mainpic > #changeimg > img').attr('src','./jpg/img'+ imgeIndex +'.jpg');
-  imgeIndex++;
-  if(imgeIndex > 2){
-    imgeIndex = 1;
-  }
-}
-$(document).ready(
-  function () {
-    $('.goto').click(
-      function () {
-        var base = $(this).attr('base');
-        var go = $(this).attr('go');
-        var where = "./html/" + base +"/" + go + ".html";
-        location.href = where;
-      }
-    );
-      setInterval("changePotalImg()",2000);
-  }
-);
 
-$.fn.toggleSub = function($target, complete)
-	{
-		var
-			$this = $(this),
-			result = {
-				self : $this,
-				target : $target
-			}
-		;
-		$this.on('click', function(){
-			$(this).toggleClass('on');
-			$(this).parent().children($target).slideToggle(200, function(){
-				$(this).parent().children($target).toggleClass('on').removeAttr('style');
-			});
-			if (complete)
-			{
-				complete(result);
-			}
+
+
+$(function () {
+	//alert("jQuery init");
+	$('#tabs > span').click(function () {
+		$('.contentmenu').css('background-color', 'white');
+		$("#tabs > span").css('z-index', '0');
+		$(this).css('z-index', '1');
+		var divname = $(this).attr('id');
+		//alert(divname);
+		$('.contentmenudiv').hide();
+		$('#' + divname + 'div').show();
+	});
+	$("#gonji").click();
+
+	const slideImg = $('#photos article'); //모든 슬라이드들
+	let currentIdx = 1; //현재 슬라이드 index
+
+	$('#perv').click(function () { // 왼쪽화살표 눌렀을때
+		currentIdx--;
+		$('#photos').css("transition", "left 0.2s ease-out");
+		$('#photos').css("left", (-currentIdx * 400) + 'px');
+
+		$("#nav > div").css("background-color", "lightgray");
+		$("#nav > div:nth-child(" + currentIdx + ")").css("background-color", "gray");
+	
+		if (currentIdx == 0) {
+			$("#nav > div").css("background-color", "lightgray");
+			$("#nav > div:nth-child(10)").css("background-color", "gray");
+			
+			setTimeout(function () {
+				currentIdx = slideImg.length - 2;
+				$('#photos').css("transition", "left 0s ease-out");
+				$('#photos').css("left", (-currentIdx * 400) + 'px');
+			}, 200);
+		}
+	});
+	$('#next').click(function () { // 오른쪽화살표를 눌렀을때 
+		currentIdx++;
+		$('#photos').css("transition", "left 0.2s ease-out");
+		$('#photos').css("left", (-currentIdx * 400) + 'px');
+
+		$("#nav > div").css("background-color", "lightgray");
+		$("#nav > div:nth-child(" + currentIdx + ")").css("background-color", "gray");
+
+		if (currentIdx == slideImg.length - 1) {
+
+			$("#nav > div").css("background-color", "lightgray");
+			$("#nav > div:nth-child(1)").css("background-color", "gray");
+
+			setTimeout(function () {
+				$('#photos').css("transition", "left 0s ease-out");
+				currentIdx = 1;
+				$('#photos').css("left", (-currentIdx * 400) + 'px');
+			}, 200);
+		}
+	});
+
+	setInterval( function(){
+		$('#next').click();
+	},3000);
+
+	for (let i = 1; i <= 10; i++) {
+		$("#nav > div:nth-child(" + i + ")").click(function () {
+			currentIdx = i;
+			$('#photos').css("transition", "left 0.2s ease-out");
+			$('#photos').css("left", (-currentIdx * 400) + 'px');
+			$("#nav > div").css("background-color", "lightgray");
+			$(this).css("background-color", "gray");
 		});
 	}
 
-	$(function(){
-		//alert("jQuery init");
+	const slideJpg = $('#mainpicc article');
+	let mainImgIdx = 1;
 
-		$('#tabs > span').click(function () {
-			$('.contentmenu').css('background-color', 'white');
-			$("#tabs > span").css('z-index', '0');
-			$(this).css('z-index', '1');
-			var divname = $(this).attr('id');
-			//alert(divname);
-			$('.contentmenudiv').hide();
-			$('#' + divname + 'div').show();
-		});
+	$('#left').click(function () { // 왼쪽화살표 눌렀을때
+		mainImgIdx--;
+		$('#mainpicc').css("transition", "left 0.2s ease-out");
+		$('#mainpicc').css("left", (-mainImgIdx * 1400) + 'px');
 
-		$("#gonji").click();
+		$("#navb > div").css("background-color", "rgba(0,0,0,0)");
+		$("#navb > div:nth-child(" + mainImgIdx + ")").css("background-color", "white");
+
+		if (mainImgIdx == 0) { 
+			$("#navb > div").css("background-color", "rgba(0,0,0,0)");
+			$("#navb > div:nth-child(2)").css("background-color", "white");
+
+			setTimeout(function () {
+				mainImgIdx = slideJpg.length - 2;
+				$('#mainpicc').css("transition", "left 0s ease-out");
+				$('#mainpicc').css("left", (-mainImgIdx * 1400) + 'px');
+			}, 200);
+		}
+
+
 	});
-	
-	
+
+	$('#right').click(function () { // 오른쪽화살표를 눌렀을때
+		mainImgIdx++;
+		$('#mainpicc').css("transition", "left 0.2s ease-out");
+		$('#mainpicc').css("left", (-mainImgIdx * 1400) + 'px');
+		$("#navb > div").css("background-color", "rgba(0,0,0,0)");
+		$("#navb > div:nth-child(" + mainImgIdx + ")").css("background-color", "white");
+		if (mainImgIdx == slideJpg.length - 1) {
+			$("#navb > div").css("background-color", "rgba(0,0,0,0)");
+			$("#navb > div:nth-child(1)").css("background-color", "white");
+			setTimeout(function () {
+				$('#mainpicc').css("transition", "left 0s ease-out");
+				mainImgIdx = 1;
+				$('#mainpicc').css("left", (-mainImgIdx * 1400) + 'px');
+			}, 200);
+		}
+	});
+
+	setInterval( function(){
+		$('#right').click();
+	},3000);
+
+
+	for (let i = 1; i <= 2; i++) {
+		$("#navb > div:nth-child(" + i + ")").click(function () {
+			mainImgIdx = i;
+			$('#mainpicc').css("transition", "left 0.2s ease-out");
+			$('#mainpicc').css("left", (-mainImgIdx * 1400) + 'px');
+			$("#navb > div").css("background-color", "rgba(0,0,0,0)");
+			$(this).css("background-color", "white");
+		});
+	}
+
+});
+
